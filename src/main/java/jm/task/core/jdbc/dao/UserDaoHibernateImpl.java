74587ac3dea38b4c8ab5c5fa09d297d5b1ddd9bd
a_khoruzhenko <khoruzhenko-ad@dep124.avt>
2023-11-10 16:42:03 +0500

    User mapped, Util implemented, Service started

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,6 +1,9 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import jm.task.core.jdbc.util.Util;
 
 import java.util.List;
 
@@ -22,7 +25,16 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Transaction transaction = null;
+        try (Session session = Util.buildSessionFactory().openSession()){
+            transaction = session.beginTransaction();
+            session.save(new User(name,lastName,age));
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 
     @Override
